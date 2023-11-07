b7a2131ae69a26f70faf1ff2e8d78b5861e6a18c
Achanandhi-M <achanandhi.m@gmail.com>
2023-11-07 13:36:18 +0530

    changes-modified

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Import route modules
+const route= require('./routes/route')
 const route1 = require('./routes/route1');
 const route2 = require('./routes/route2');
 
-// Define route handlers
+// Define route handler
+app.use('/',route);
 app.use('/route1', route1);
 app.use('/route2', route2);
 
