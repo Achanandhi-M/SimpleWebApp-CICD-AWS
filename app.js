051afcd084b536df8c587d5e1d9dc9baad0facf2
Achanandhi-M <achanandhi.m@gmail.com>
2023-11-07 13:48:36 +0530

    files-modified

--- app.js
+++ app.js
@@ -5,12 +5,10 @@ const port = process.env.PORT || 3000;
 // Import route modules
 const defaultRoute= require('./routes/default')
 const route1 = require('./routes/route1');
-const route2 = require('./routes/route2');
 
 // Define route handler
 app.use('/',defaultRoute);
 app.use('/route1', route1);
-app.use('/route2', route2);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
